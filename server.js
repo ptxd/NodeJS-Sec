85684f18d54179547af82694721791864dc9e8c4
ptxd <ptxdas@gmail.com>
2017-10-15 20:59:41 -0700

    sec-0.1

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+'use strict'
 const express = require('express')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 const logger = require('morgan')
 
 //** Initialize express ---------**
@@ -10,32 +12,32 @@ const port = process.env.port || 8080
 
 //** Initialize Middleware --------- */
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:false}))
 
 //Log requests to server console
-app.use(logger(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
+app.use(logger(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'))
 
 //** Endpoints ---------**
 
 //** Server testing endpoints ---------**
 app.get('/getTest',function(req,res){
-    console.log('req');
-    return res.status(200).send(`test successful`);
+    console.log('req')
+    return res.status(200).send(`test successful`)
 });
 
 app.post('/postTest',function(req,res){
-    console.log(req);
-    return res.status(200).send(`test successful`);
+    console.log(req)
+    return res.status(200).send(`test successful`)
 });
 
 app.put('/putTest',function(req,res){
-    console.log(req);
-    return res.status(200).send(`test successful`);
+    console.log(req)
+    return res.status(200).send(`test successful`)
 });
 
 app.delete('/delTest',function(req,res){
-    console.log('req');
-    return res.status(200).send(`test successful`);
+    console.log('req')
+    return res.status(200).send(`test successful`)
 });
 
-app.listen(port,()=>console.log(`Listening on port ${port}`));
+app.listen(port,()=>console.log(`Listening on port ${port}`))
